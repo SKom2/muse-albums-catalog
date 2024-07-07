979c73523bc6a36b3174acb5d48cce09d3bd9af0
san4e <san4es.kom@gmail.com>
2024-07-07 12:53:34 +0300

    feat: setting up aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/muse-catalog',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/assets': '/src/assets',
+      '@/components': '/src/components',
+      '@/containers': '/src/containers',
+      '@/hooks': '/src/hooks',
+      '@/layouts': '/src/layouts',
+      '@/pages': '/src/pages',
+      '@/routes': '/src/routes',
+      '@/services': '/src/services',
+      '@/ui': '/src/ui',
+    },
+  },
 })
