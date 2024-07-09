af79a2880d028284e7422746088885574d2ced92
san4e <san4es.kom@gmail.com>
2024-07-09 12:45:02 +0300

    fix: some changes with file paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/muse-catalog',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@/assets': '/src/assets',
-      '@/components': '/src/components',
-      '@/containers': '/src/containers',
-      '@/hooks': '/src/hooks',
-      '@/layouts': '/src/layouts',
-      '@/pages': '/src/pages',
-      '@/routes': '/src/routes',
-      '@/services': '/src/services',
-      '@/ui': '/src/ui',
-    },
-  },
+  plugins: [react(), tsconfigPaths()]
 })
